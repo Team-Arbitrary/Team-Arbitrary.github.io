da0fb900013c62c4bbd1dd3011986571ea808663
Josie <josiehenvey@gmail.com>
2022-03-27 21:57:42 -0400

    init

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php //header.php 
+// TODO: Add session initialization for logins
+?>
+<!doctype HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/general.css">
+    </head>
+    <body>
+        <header>
+            <!-- TODO: Add nav w/ links to login, professional-dev form, etc... -->
+            <h2 class="text-xl margin-y-0 padding-1">
+                Professional Development tool
+            </h2>
+        </header>
--- pdCompletionForm.php
+++ pdCompletionForm.php
@@ -1,53 +1,62 @@
+<?php //pdCompletionForm.php
+    require 'header.php';
+?>
 
-<!-- PROFESSIONAL DEVELOPMENT LOG FORM -->
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <h1>Professional Development Completion Form</h1>
-        <form method="post" action="pdCompletionForm.php">
-            <!-- EMPLOYEE STATUS RADIO BUTTONS -->
-            <div class="employeeStatus">
-                <p>What is your employee status?</p>
-                <input type="radio" name="status" id="full_time" value="full_time">
-                <label for="status">Full-time Faculty Member</label><br>
-                <input type="radio" name="status" id="adjunct" value="adjunct">
-                <label for="status">Adjunct Faculty Member</label><br>
-            </div><br>
+<section class="container">
+    <h1>Professional Development Completion Form</h1>
 
-            <!-- TEXT BOX FOR SESSION NAME --> 
-            <div class="sessionName">
-                <label for="session">What professional development session or event did you attend?</label>
-                <input type="text" name="session" size="30" maxlength="50"><br>
-            </div><br>
+    <!-- TODO: This action should point to a file that handles the $_POST superglobal
+                by testing if all fields are set, and if so, make a SQL call to add
+                this submission as a row. -->
+    <form method="post" action="pdCompletionForm.php">
+        <!-- EMPLOYEE STATUS RADIO BUTTONS -->
+        <div class="employeeStatus">
+            <p>What is your employee status?</p>
+            <input type="radio" name="status" id="full_time" value="full_time">
+            <label for="status">Full-time Faculty Member</label><br>
+            <input type="radio" name="status" id="adjunct" value="adjunct">
+            <label for="status">Adjunct Faculty Member</label><br>
+        </div><br>
 
-            <!-- START DATE OF SESSION -->
-            <div class="sessionStartDate">
-                <label for="date">What day did this session begin?</label>
-                <input type="date" name="startDate"><br>
-            </div><br>
+        <!-- TEXT BOX FOR SESSION NAME --> 
+        <div class="sessionName">
+            <label for="session">What professional development session or event did you attend?</label>
+            <input type="text" name="session" size="30" maxlength="50"><br>
+        </div><br>
 
-            <!-- END DATE OF SESSION -->
-            <div class="sessionEndDate">
-                <label for="date">What day did this session end?</label>
-                <input type="date" name="endDate"><br>
-            </div><br>
+        <!-- START DATE OF SESSION -->
+        <div class="sessionStartDate">
+            <label for="date">What day did this session begin?</label>
+            <input type="date" name="startDate"><br>
+        </div><br>
 
-            <!-- ORGANIZATION -->
-            <div class="sessionOrg">
-                <label for="org">Which organization hosted this session?</label>
-                <input type="text" name="org" size="30" maxlength="50"><br>
-            </div><br>
+        <!-- END DATE OF SESSION -->
+        <div class="sessionEndDate">
+            <label for="date">What day did this session end?</label>
+            <input type="date" name="endDate"><br>
+        </div><br>
 
-            <!-- PRESENTER -->
-            <div class="sessionPresenter">
-                <label for="presenter">Who was the main presenter?</label>
-                <input type="text" name="presenter" size="30" maxlength="50"><br>
-            </div><br>
+        <!-- ORGANIZATION -->
+        <div class="sessionOrg">
+            <label for="org">Which organization hosted this session?</label>
+            <input type="text" name="org" size="30" maxlength="50"><br>
+        </div><br>
 
-            <!-- DESCRIPTION -->
-            <div class="sessionDescription">
-                <label for="description">Describe the session you attended and how it relates to your professional development.</label><br>
-                <textarea name="description" rows="5" cols="80"></textarea>
-            </div><br>
-     </body>
-</html>
+        <!-- PRESENTER -->
+        <div class="sessionPresenter">
+            <label for="presenter">Who was the main presenter?</label>
+            <input type="text" name="presenter" size="30" maxlength="50"><br>
+        </div><br>
+
+        <!-- DESCRIPTION -->
+        <div class="sessionDescription">
+            <label for="description">Describe the session you attended and how it relates to your professional development.</label><br>
+            <textarea name="description" rows="5" cols="80"></textarea>
+        </div><br>
+</form>
+
+</section>
+
+<?php 
+    require 'footer.php';
+?>
