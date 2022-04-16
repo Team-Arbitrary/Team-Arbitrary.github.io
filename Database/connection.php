ae2e4c6628eb0741d655153b6a15f5a19b63c387
fengyuc <zxcv1050135042@126.com>
2022-04-16 09:23:58 -0400

    Update connection.php

--- Database/connection.php
+++ Database/connection.php
@@ -1,9 +1,21 @@
 <?php
 // Configuration
 $serverName = "localhost";
-$username = "id18780626_arbitrary_tests";
-$password = "k6wBr^9q@ShCt1Ns";
-$databaseNAME = "id18780626_arbitrary_tests_database";
+
+// Local Website
+$username = "admin";
+$password = "zxcv7373";
+$databaseNAME = "arbitrary_team_database";
+
+// Testing Website
+//$username = "id18780626_arbitrary_tests";
+//$password = "k6wBr^9q@ShCt1Ns";
+//$databaseNAME = "id18780626_arbitrary_tests_database"
+
+// Project Website
+//$username = "id18737052_projectschema";
+//$password = "[vCQ=VZL\bR9n(m^";
+//$databaseNAME = "id18737052_project_schema"
 
 require_once "../utils.php";  // Load some common functions to reuse code
 
